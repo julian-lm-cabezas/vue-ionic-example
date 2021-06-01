941455804d082983b19ab14f89c7d456b1cbac14
julian-lm-cabezas <julian-harry.cabezas@ext.leroymerlin.es>
2021-06-01 12:32:40 +0200

    add child component

---
+++ src/components/OrderList.vue
@@ -0,0 +1,11 @@
+<template>
+    <ion-list>
+        <ion-item>wololo</ion-item>
+    </ion-list>
+</template>
+<script>
+import { IonList, IonItem} from '@ionic/vue'
+export default {
+    components: { IonList, IonItem}
+}
+</script>
--- src/views/OrdersPage.vue
+++ src/views/OrdersPage.vue
@@ -5,12 +5,15 @@
                 <ion-title>ORDERS</ion-title>
             </ion-toolbar>
         </ion-header>
-        <ion-content></ion-content>
+        <ion-content>
+            <order-list/>
+        </ion-content>
     </ion-page>
 </template>
 <script>
 import {IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue'
+import OrderList from '@/components/OrderList.vue'
 export default {
-    components:{IonPage, IonHeader,IonToolbar, IonTitle, IonContent }
+    components:{IonPage, IonHeader,IonToolbar, IonTitle, IonContent, 'order-list':OrderList }
 }
 </script>
