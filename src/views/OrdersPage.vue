9d63c1ab0432a1d1a612f56eec8cb0551f68b99f
julian-lm-cabezas <julian-harry.cabezas@ext.leroymerlin.es>
2021-06-01 13:51:16 +0200

    ion slide use

--- src/views/OrdersPage.vue
+++ src/views/OrdersPage.vue
@@ -9,28 +9,31 @@
             <ion-grid>
                 <ion-row >
                     <ion-col size-md="3" size-xs="6" offset-md="3">
-                        <ion-item @click="vue3=false">
+                        <ion-item @click="loadList(false)">
                             <ion-label> OptionsAPI (vue2)</ion-label>
                             <ion-checkbox :checked="!vue3" :disabled="!vue3"/>
                         </ion-item>
                     </ion-col>
                     <ion-col size-md="3" size-xs="6">
-                        <ion-item @click="vue3=true">
+                        <ion-item @click="loadList(true)">
                             <ion-label> CompositionAPI (vue3)</ion-label>
                             <ion-checkbox :checked="vue3" :disabled="vue3"/>
                         </ion-item>
                     </ion-col>
                 </ion-row>
             </ion-grid>
-            <order-list v-if="!vue3" :orders="orders"/>
+            <ion-slides pager="false" :options="{ initialSlide: 1, speed: 400 }">
+                <ion-slide><order-list :orders="orders"/></ion-slide>
+                <ion-slide><order-list :orders="orders"/></ion-slide>
+            </ion-slides>
         </ion-content>
     </ion-page>
 </template>
 <script>
-import {IonPage, IonHeader, IonToolbar, IonTitle, IonContent,IonGrid, IonRow,IonCol,IonItem, IonLabel, IonCheckbox } from '@ionic/vue'
+import {IonPage, IonHeader, IonToolbar, IonTitle, IonContent,IonGrid, IonRow,IonCol,IonItem, IonLabel, IonCheckbox,IonSlides, IonSlide } from '@ionic/vue'
 import OrderList from '@/components/OrderList.vue'
 export default {
-    components:{IonPage, IonHeader,IonToolbar, IonTitle, IonContent,IonGrid, IonRow,IonCol, IonItem, IonLabel, IonCheckbox, 'order-list':OrderList },
+    components:{IonPage, IonHeader,IonToolbar, IonTitle, IonContent,IonGrid, IonRow,IonCol, IonItem, IonLabel, IonCheckbox,IonSlides, IonSlide, 'order-list':OrderList },
     data: ()=>{
         return {
             vue3: false,
@@ -39,7 +42,19 @@ export default {
                 {id:2, clientId: 1234, date: new Date().toISOString(), price: 107.11 },
                 {id:3, clientId: 4567, date: new Date().toISOString(), price: 45.20 },
                 {id:4, clientId: 7788, date: new Date().toISOString(), price: 12.88 }
-            ]
+            ],
+            slider: null
+        }
+    },
+    async mounted(){
+        (await document.querySelector('ion-slides').getSwiper()).allowTouchMove=false
+        
+    },
+    methods:{
+        loadList(bool){
+            console.log(bool)
+            this.vue3 = bool
+            document.querySelector('ion-slides').slideTo(bool?0:1)
         }
     }
 }
